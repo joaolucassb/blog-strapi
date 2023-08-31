3d083220032ffdfdafded697cb2242d9561abde1
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-08-31 17:20:26 -0300

    change config of findOne Controller

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -10,5 +10,15 @@ module.exports = createCoreController('api::post.post', {
   count(ctx) {
     var { query } = ctx.request
     return strapi.query('api::post.post').count({ where: query});
+  },
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const response  = await strapi.db.query('api::post.post').findOne({
+      where: {slug}
+    })
+
+    return response;
   }
 });
