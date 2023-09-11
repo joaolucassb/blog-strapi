9fa9b50dcca6b73afd3b9d5542bf98e0b824c0e4
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-09-11 19:34:28 -0300

    change post controller

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,10 +1,28 @@
-'use strict';
+// @ts-nocheck
+"use strict";
 
 /**
- * post controller
+ *  post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::post.post", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(post);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
 
 module.exports = createCoreController('api::post.post', {
   count(ctx) {
