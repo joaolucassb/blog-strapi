a7a9566b73c8734caec2e81482dbbf60b7e391dd
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-09-11 17:21:19 -0300

    fix post router

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -11,19 +11,4 @@ module.exports = createCoreController('api::post.post', {
     var { query } = ctx.request
     return strapi.query('api::post.post').count({ where: query});
   },
-
-  async findOne(ctx) {
-    const { slug } = ctx.params;
-
-    const response  = await strapi.db.query('api::post.post').findOne({
-      where: {slug},
-        populate: [
-          'cover',
-          'author',
-          'category',
-        ],
-    })
-
-    return response;
-  }
 });
