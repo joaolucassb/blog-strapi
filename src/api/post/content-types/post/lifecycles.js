9d960c3a467ae86fd9be95254886cfe54e0be9c9
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-09-22 11:47:50 -0300

    add lifecyles-hook

---
+++ src/api/post/content-types/post/lifecycles.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+module.exports = {
+   async afterCreate(result, data) {
+      axios.post('https://api.netlify.com/build_hooks/650da45747c6d7072c088056');
+   },
+
+   async afterUpdate(result, params, data) {
+    axios.post('https://api.netlify.com/build_hooks/650da45747c6d7072c088056');
+  },
+}
